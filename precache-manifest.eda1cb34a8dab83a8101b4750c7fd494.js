3b6f383725942bd87d852596aa2100be360ab23d
Mangesh N - iProgrammer <mangeshn@iprogrammer.com>
2020-04-03 22:26:45 +0530

    Updates

---
+++ precache-manifest.eda1cb34a8dab83a8101b4750c7fd494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c95fa15126d99f578f7c299ca2252cb",
+    "url": "/react-calculator-app/index.html"
+  },
+  {
+    "revision": "f4875f48fc4b9328c78a",
+    "url": "/react-calculator-app/static/css/main.720f72da.chunk.css"
+  },
+  {
+    "revision": "b475d6ec738bbc3e7552",
+    "url": "/react-calculator-app/static/js/2.e36a4552.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calculator-app/static/js/2.e36a4552.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4875f48fc4b9328c78a",
+    "url": "/react-calculator-app/static/js/main.d22a2f66.chunk.js"
+  },
+  {
+    "revision": "b93675aed4a6c5b4a992",
+    "url": "/react-calculator-app/static/js/runtime-main.72b7aa91.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-calculator-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
